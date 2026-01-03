5b15eea21db9c6c7323ea801d0537fa6fcf3dc71
Wddptesting <159699720+Wddptesting@users.noreply.github.com>
2026-01-03 19:42:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,11 +44,12 @@ def nacitaj_data():
 
         # Konverzia dátumu – robustná verzia
         if "Datum" in df.columns:
-    df["Datum_date"] = pd.to_datetime(
-        df["Datum"].astype(str).str.strip(),
-        dayfirst=True,
-        errors="coerce"
-    ).dt.tz_localize(None)  
+            df["Datum_date"] = pd.to_datetime(
+                df["Datum"].astype(str).str.strip(),
+                dayfirst=True,  # deň je prvé číslo
+                errors="coerce"
+            ).dt.tz_localize(None)
+ 
      
         # Konverzia čísel
         num_cols = ["Rano", "Vybery", "Vecer", "Cista_Trzba", "Rok"]
